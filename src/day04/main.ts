0656309cff2dadbf9cfd22f0393f948664702820
Pascal Vermeulen <pas.verm@gmail.com>
2022-12-04 13:40:14 +0100

    solve day4

--- src/day04/main.ts
+++ src/day04/main.ts
@@ -2,19 +2,67 @@ import chalk from "chalk";
 import { Challenge } from "../types";
 import { getFileContent } from "../utils";
 
-function main() {
-    const fileContent = getFileContent(__dirname, "input.txt");
+class Range {
+    constructor(public start: number, public end: number) {}
+
+    contains(other: Range) {
+        return this.start <= other.start && this.end >= other.end;
+    }
+
+    overlaps(other: Range) {
+        return this.start <= other.start && this.end >= other.start
+            || this.start >= other.start && this.start <= other.end;
+    }
+}
+
+function getRanges() {
+    const fileContent = getFileContent(__dirname, "input.txt")!;
+    return fileContent.trim().split("\n").map(parseLine);
+}
+
+function parseRange(range: string): Range {
+    const [start, end] = range.split("-").map(Number);
+    return new Range(start, end);
+}
+
+function parseLine(line: string): [Range, Range] {
+    const [range1, range2] = line.trim().split(",");
+    return [parseRange(range1), parseRange(range2)];
+}
+
+function countFullyContained(ranges: [Range, Range][]) {
+    return ranges.reduce((sum, [rangeA, rangeB]) => {
+        return rangeA.contains(rangeB) || rangeB.contains(rangeA) ? sum + 1 : sum;
+    }, 0);
+}
+
+function countOverlapping(ranges: [Range, Range][]) {
+    return ranges.reduce((sum, [rangeA, rangeB]) => {
+        return rangeA.overlaps(rangeB) ? sum + 1 : sum;
+    }, 0);
+}
+
+function partOne() {
+    const ranges = getRanges();
+    const result = countFullyContained(ranges);
+    console.log("Fully contained ranges:", result);
+}
+
+function partTwo() {
+    const ranges = getRanges();
+    const result = countOverlapping(ranges);
+    console.log("Overlapping ranges:", result);
 }
 
 export const day4: Challenge = {
     day: 4,
-    title: "",
+    title: "Camp Cleanup",
     run: [{
             part: 1,
-            run: () => console.log(chalk.yellow("Challenge part not yet implemented")),
+            run: partOne
         }, {
             part: 2,
-            run: () => console.log(chalk.yellow("Challenge part not yet implemented")),
+            run: partTwo
         }
     ]
 }
