3c1f2f7c3f1290d6d061c91d73181b26b44724f7
Pascal Vermeulen <pas.verm@gmail.com>
2022-12-06 21:00:23 +0100

    solve day6

--- src/day06/main.ts
+++ src/day06/main.ts
@@ -2,19 +2,38 @@ import chalk from "chalk";
 import { Challenge } from "../types";
 import { getFileContent } from "../utils";
 
-function main() {
-    const fileContent = getFileContent(__dirname, "input.txt");
+function readStream() {
+    const fileContent = getFileContent(__dirname, "input.txt")!;
+    return [...fileContent.trim()];
+}
+
+function findMarker(buffer: string[], n: number) {
+    const previousLetters = new Set<string>();
+    let left = 0;
+    let right = 0;
+    while (right < buffer.length) {
+        while (previousLetters.has(buffer[right])) {
+            previousLetters.delete(buffer[left]);
+            left += 1;
+        }
+        previousLetters.add(buffer[right]);
+        if (previousLetters.size === n) {
+            console.log(`Found marker at position ${right + 1}`);
+            return;
+        }
+        right += 1;
+    }
 }
 
 export const day6: Challenge = {
     day: 6,
-    title: "",
+    title: "Tuning Trouble",
     run: [{
             part: 1,
-            run: () => console.log(chalk.yellow("Challenge part not yet implemented")),
+            run: () => findMarker(readStream(), 4)
         }, {
             part: 2,
-            run: () => console.log(chalk.yellow("Challenge part not yet implemented")),
+            run: () => findMarker(readStream(), 14),
         }
     ]
 }
